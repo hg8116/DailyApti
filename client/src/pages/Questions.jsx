2d7162ae1c4517119af4f67981934154d2c070e1
Harsh Gupta <2011.harshgupta@gmail.com>
2023-06-12 14:46:57 +0530

    Production API update on frontend

--- client/src/pages/Questions.jsx
+++ client/src/pages/Questions.jsx
@@ -2,7 +2,8 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://dailyaptibackend.onrender.com/api",
 })
 
 const Questions = () => {
